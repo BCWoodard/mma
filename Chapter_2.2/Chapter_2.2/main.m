468b52366766a2c6ce7d016dc6b125b78909b521
Brad Woodard <bcwoodard@gmail.com>
2013-07-04 15:41:00 -0500

    Added Chapter 2.2 - Line comments

---
+++ Chapter_2.2/Chapter_2.2/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  Chapter_2.2
+//
+//  Created by Brad Woodard on 7/4/13.
+//  Copyright (c) 2013 Brad Woodard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>  // Points to the Foundation Framework
+
+int main(int argc, const char * argv[]) // This tells the program where to start.
+                                        // main is included in every project
+{                                       // Opening statement block -  the beginning of main
+
+    @autoreleasepool {                  // autoreleasepool deals with memory management
+        
+        // insert code here...
+        NSLog(@"Hello, World!");        //  Writes "Hello, World!" to the console
+        
+    }                                   // Closing statement block
+    return 0;                           // Final line of main and says "main was successful"
+}                                       // Final closing statement block
+
