cc752e21dd2bb371211fc3f8336044dd5cab7608
Brad Woodard <bcwoodard@gmail.com>
2013-07-04 15:13:37 -0500

    Added Hello World project

---
+++ Chapter_1.2/Chapter_1.2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Chapter_1.2
+//
+//  Created by Brad Woodard on 7/4/13.
+//  Copyright (c) 2013 Brad Woodard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
