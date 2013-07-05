4bc52b7eb545f295d783247ad88c0e0b4a0f5e01
Brad Woodard <bcwoodard@gmail.com>
2013-07-04 19:04:14 -0500

    Completed challenge 3 thru chapter 3

--- Chapter_2.2/Chapter_2.2/main.m
+++ Chapter_2.2/Chapter_2.2/main.m
@@ -8,16 +8,14 @@
 
 #import <Foundation/Foundation.h>  // Points to the Foundation Framework
 
-int main(int argc, const char * argv[]) // This tells the program where to start.
-                                        // main is included in every project
-{                                       // Opening statement block -  the beginning of main
+int main(int argc, const char * argv[])
+{                                       
 
-    @autoreleasepool {                  // autoreleasepool deals with memory management
-        
-        // insert code here...
-        NSLog(@"Hello, World!");        //  Writes "Hello, World!" to the console
-        
-    }                                   // Closing statement block
-    return 0;                           // Final line of main and says "main was successful"
-}                                       // Final closing statement block
+    @autoreleasepool {
+        if (condition == true) {
+            // comments go here
+        }
+    }
+    return 0;
+}
 
