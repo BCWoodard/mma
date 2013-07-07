f400c7f43aee0816043734cd5194fb339ee6b2d7
Brad Woodard <bcwoodard@gmail.com>
2013-07-07 15:01:14 -0500

    Added BNR - A Good Start

---
+++ BNR/AGoodStart/AGoodStart/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Brad Woodard on 7/7/13.
+//  Copyright (c) 2013 Brad Woodard. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /* Is that actually any good?
+     Maybe it needs a rewrite */
+    
+    return 0;
+}
+
